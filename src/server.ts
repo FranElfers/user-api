6f9ce388486f07c342a5ead9b47aca24b8c8becc
franelfers <fransozzan@gmail.com>
2026-03-17 00:44:35 -0300

    Obtain every index daily by Cronjob

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import app from "./app";
 import { connectDB } from "./config/database";
+import { startIndexCronJob } from './indexes';
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 async function start() {
   await connectDB();
+  startIndexCronJob()
 
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
